aff565a2ff6439f9fe5b642fc7fec0f00e6f1b10
aleksnick <nikolaev@iprojects.ru>
2026-08-20 15:51:20 +0300

    feat: validate strategy configuration

--- src/CupAndHandle/strategy.ts
+++ src/CupAndHandle/strategy.ts
@@ -1,11 +1,16 @@
-import type { StrategyRegistryEntry } from "@tradejs/types";
+import { createStrategyConfigParser } from "@tradejs/strategy-kit/config";
+import type { ValidatedStrategyRegistryEntry } from "@tradejs/strategy-kit/config";
 import { config as DEFAULT_CONFIG, CupAndHandleConfig } from "./config";
 import { createCupAndHandleCore } from "./core";
 import { cupAndHandleManifest } from "./manifest";
 
-export const CupAndHandleStrategyDefinition: StrategyRegistryEntry<CupAndHandleConfig> =
+export const CupAndHandleStrategyDefinition: ValidatedStrategyRegistryEntry<CupAndHandleConfig> =
   {
     defaults: DEFAULT_CONFIG,
+    parseConfig: createStrategyConfigParser({
+      strategyName: "CupAndHandle",
+      defaults: DEFAULT_CONFIG,
+    }),
     createCore: createCupAndHandleCore,
     manifest: cupAndHandleManifest,
   };
